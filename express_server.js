29001b17af5f5309ab39c54a6ec2638cd0995ab2
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-25 13:50:27 -0700

    Add post method to handle delete button

--- express_server.js
+++ express_server.js
@@ -49,6 +49,10 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls"); 
+});
 
 app.listen(PORT, () => {
   console.log(`Tinyapp listening on port ${PORT}!`);
