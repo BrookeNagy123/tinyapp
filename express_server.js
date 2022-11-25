ad99d736c4cc1f00f4733211cc68324ec60f08d4
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-25 08:58:37 -0700

    longURL in urls/:id updated

--- express_server.js
+++ express_server.js
@@ -17,10 +17,18 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]  }; //* Update the longURL: THIS PIECE */
+  res.render("urls_show", templateVars);
+});
+
+
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tinyapp listening on port ${PORT}!`);
 });
